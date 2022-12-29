89617b31cef84ef9bfb10b8ec161b4dd78a86b83
Ruslan <rvslan@pm.me>
2022-12-29 21:43:50 +0100

    Fixed property

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
     <motion.div
       variants={staggerContainer}
-      initial='hiden'
+      initial='hidden'
       whileInView='show'
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
