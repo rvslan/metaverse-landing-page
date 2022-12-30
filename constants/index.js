6ac16073cfe7e0d74e52eb82571bcc23230708eb
Ruslan <rvslan@pm.me>
2022-12-30 15:55:28 +0100

    Insights section init

--- constants/index.js
+++ constants/index.js
@@ -49,18 +49,21 @@ export const newFeatures = [
 
 export const insights = [
   {
+    id: 1,
     imgUrl: '/planet-06.png',
     title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
     subtitle:
         'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
   {
+    id: 2,
     imgUrl: '/planet-07.png',
     title: '7 tips to easily master the madness of the Metaverse',
     subtitle:
         'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
   },
   {
+    id: 3,
     imgUrl: '/planet-08.png',
     title: 'With one platform you can explore the whole world virtually',
     subtitle:
